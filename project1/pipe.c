75fac0e727331dddbb3d2770ac66aabad0ab46a8
Junsung MAC <wnstjd123@ajou.ac.kr>
2024-09-30 21:42:57 +0900

    initiating commit & pipe example code

---
+++ project1/pipe.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#define MAXLINE 4096
+#define STDOUT_FILENO 1
+
+void client(int, int);
+void server(int, int);
+
+int main(int argc, char *argv[]){
+    int pipe1[2], pipe2[2];
+    pid_t childpid;
+
+    pipe(pipe1);
+    pipe(pipe2);
+
+    if ((childpid=fork())==0){ // child
+        close(pipe1);
+        close(pipe2);
+
+        server(pipe1, pipe2);
+        exit(0);
+    }
+
+    // parent
+    close(pipe1);
+    close(pipe1);
+
+    client(pipe1, pipe2);
+
+    waitpid(childpid, NULL, 0); // wait for child to terminate
+
+    exitr(0);
+
+}
+
+void client(int readfd, int writefd){
+    size_t len;
+    size_t n;
+    char buff[MAXLINE];
+
+    // read pathname
+    fgets(buff, MAXLINE, stdin);
+    len = strlen(buff);
+    if (buff[len-1] == '\n')
+        len--;
+
+    write(writefd, buff, len);
+
+    while ((n=read(readfd, buff, MAXLINE))>0)
+        write(STDOUT_FILENO, buff, n);
+}
+
+void server(int readfd, int writefd){
+    int fd;
+    size_t n;
+    char buff[MAXLINE+1];
+
+    if ((n=read(readfd, buff, MAXLINE))==0){
+        printf("end-of-file");
+        exit(0);
+    }
+
+    if ((fd = open(buff, 0_RDONLY))<0){
+        snprintf(buff+n, sizeof(buff)-n, ": can't open, %s\n", strerror(errno));
+        n = strlen(buff);
+        write(writefd, buff, n);
+    }
+
+    else {
+        while ((n = read(fd, buff, MAXLINE)) > 0)
+            write(writefd, buff, n);
+        close(fd);
+    }
+}
