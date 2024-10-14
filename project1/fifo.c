dd16d78d73036232b05b8a855269fbdae1b738e4
Junsung MAC <wnstjd123@ajou.ac.kr>
2024-10-14 21:07:22 +0900

    edit header files

--- project1/fifo.c
+++ project1/fifo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <unistd.h>
--- project1/pipe.c
+++ project1/pipe.c
@@ -3,7 +3,6 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <string.h>
 #include <sys/wait.h>
 
