4f7626e68ccb1c15d8c9ceb6b0e441e6f58c464f
Junsung MAC <wnstjd123@ajou.ac.kr>
2024-10-28 03:12:46 +0900

    add comments on client.c and server.c

--- project1/client.c
+++ project1/client.c
@@ -2,16 +2,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include <string.h>
 
 #define SIZE 512
 #define TRUE 1
-#define FALSE 0
 
 #define PIPE "./namedpipe"
 
-int main(int argc, char* argv[]){
+int main(void){
     int named_pipe;
     int byte;
     int file_length;
@@ -22,7 +20,10 @@ int main(int argc, char* argv[]){
     char buff[SIZE] = {0};
     char terminate;
 
-    if ((named_pipe = open(PIPE, O_RDWR)) == -1){
+    // Opening named pipe
+    if ((named_pipe = open(PIPE, O_RDWR)) < 0){
+
+        // when it fails
         printf("Failed to open named pipe\n");
         exit(1);
     }
@@ -30,131 +31,208 @@ int main(int argc, char* argv[]){
     printf("----------client----------\n\n");
 
     while (TRUE){
+
+        // when the named pipe doesn't exist
+        if (access(PIPE, F_OK) < 0)
+            exit(1);
+
+        // type whether user wants to terminate this program or not
         printf("do you want to terminate this code?(y: yes, n: no): ");
         scanf("%c", &terminate);
         while (getchar() != '\n');
 
+        // If user wants to terminate this program
         if (terminate == 'y'){
-            printf("Terminating server and client\n");
+            printf("Terminating the server and the client\n");
+
+            // send termination signal to the server
             while (write(named_pipe, "terminate\n", sizeof("terminate\n")) < 0);
             printf("----------terminating client----------\n\n");
+
+            // break the loop
             break;
         }
 
+        // when the termination request type is "n", pass
+        else if (terminate == 'n'){
+            NULL;
+        }
+
+        // neither "y" nor "n"
+        else{
+            printf("You typed improper temination request \n");
+            continue;
+        }
+
+        // receiving the file name
         printf("Enter the file name: ");
         fgets(file_name, SIZE, stdin);
-        file_name[strlen(file_name) - 1] = '\0';
+        file_name[strlen(file_name) - 1] = '\0'; // change new line to eof
 
+        // receiving file access mode
         printf("Type the file access type (r: read, w: write): ");
         scanf("%c", &file_mode);
-        while(getchar() != '\n');
+        while(getchar() != '\n'); // eliminating new lines
 
+        // when the access mode is "read"
         if (file_mode == 'r'){
+
+            // receiving the number of bytes to read
             printf("Enter the number of bytes to read: ");
             scanf("%d", &byte);
-            while (getchar() != '\n');
+            while (getchar() != '\n'); // eliminating new lines
 
+            // send request signal to the server
             if ((write(named_pipe, "request\n", SIZE)) < 0) {
-                printf("failed to request to server \n");
+
+                // when it fails
+                printf("failed to request to the server \n");
                 exit(1);
             }
 
             sleep(1);
 
+            // send access mode to the server
             if ((write(named_pipe, &file_mode, sizeof(char))) < 0){
+
+                // when it fails
                 printf("failed to send file mode \n");
                 exit(1);
             }
 
             sleep(1);
 
+            // send the number of bytes to read to the server
             if ((write(named_pipe, &byte, sizeof(int))) < 0){
+
+                // when it fails
                 printf("failed to send number of bytes to read \n");
                 exit(1);
             }
 
             sleep(1);
 
+            // send the file name to the server
             if ((write(named_pipe, file_name, SIZE)) < 0){
+
+                // when it fails
                 printf("failed to send file name \n");
                 exit(1);
             }
 
             sleep(2);
 
+            // receiving the result signal from the server
+            // when it fails, receives "fail"
+            // when it doesn't fail, receives what server read
             if ((read(named_pipe, buff, SIZE)) < 0){
+
+                // when it fails
                 printf("failed to receive data from the server \n");
                 exit(2);
             }
 
             sleep(1);
 
+            // when the received result is fail: user typed file name is not on this directory
             if (!(strcmp(buff, "fail\n"))){
                 printf("There's no file named %s \n", file_name);
             }
 
             else {
+
+                // when the received result is not fail: user typed file name is on this directory
+                // print what server read
                 printf("%s\n", buff);
             }
         }
 
+        // when the access mode is "write"
         else if (file_mode == 'w'){
+
+            // receiving what user wants to write
             printf("Type the string to write: ");
             fgets(data_string, SIZE, stdin);
-            data_string[strlen(data_string)-1] = '\0';
+            data_string[strlen(data_string)-1] = '\0'; // changes new line to eof
 
+            // send request signal to the server
             if ((write(named_pipe, "request\n", SIZE)) < 0) {
-                printf("failed to request to server \n");
+
+                // when it fails
+                printf("failed to request to the server \n");
                 exit(1);
             }
 
             sleep(1);
 
+            // send access mode to the server
             if ((write(named_pipe, &file_mode, sizeof(char))) < 0) {
+
+                // when it fails
                 printf("Failed to send file mode to server \n");
                 exit(1);
             }
 
+            // send file name to the server
             if ((write(named_pipe, file_name, SIZE)) < 0) {
+
+                // when it fails
                 printf("failed to send file name\n");
                 exit(1);
             }
 
             sleep(2);
 
+            // send user typed data string to the server
             if ((write(named_pipe, data_string, SIZE)) < 0){
+
+                // when it fails
                 printf("Failed to write data string \n");
                 exit(1);
             }
 
             sleep(2);
 
+            // receiving the result from the server
+            // when it fails, receives "fail"
+            // when it doesn't fail, receives the length of the file written
             if (read(named_pipe, buff, SIZE) < 0){
+
+                // when it fails
                 printf("failed to receive data from the server \n");
                 exit(2);
             }
 
             sleep(1);
 
+            // when the received result is fail: user typed file name is not on this directory
             if (!(strcmp(buff, "fail\n"))){
                 printf("There's no file named %s \n", file_name);
             }
 
+            // when the received result is not fail: user typed file name is on this directory
             else {
+
+                // receives the length of the file written
                 if (read(named_pipe, &file_length, sizeof(int)) < 0){
-                    printf("Failed to receive the length of the file \n");
+
+                    // when it fails
+                    printf("Failed to receive the length of the file written \n");
                     exit(2);
                 }
 
-                printf("received the length of the file: %d \n", file_length);
+                // print the length of the file written
+                printf("received the length of the file written: %d \n", file_length);
             }
 
         }
 
+        // when user types improper access mode
         else{
             printf("You typed improper mode \n");
         }
 
+        // initialization
         memset(file_name, 0, SIZE * sizeof(char));
         memset(&file_mode, 0, sizeof(char));
         memset(buff, 0, SIZE * sizeof(char));
--- project1/server.c
+++ project1/server.c
@@ -13,11 +13,11 @@
 int main(void){
     int named_pipe;
     int number_of_reading = 0;
-    int bytes_of_file_read = 0;
     int bytes_of_file_write = 0;
+    int bytes_of_file_read;
     int fd; // file descriptor
 
-    pid_t child;
+    pid_t child; // for fork
 
     char received_file_name[SIZE] = {0};
     char access_mode;
@@ -47,6 +47,7 @@ int main(void){
     while (TRUE){
         // memset(buff, 0, SIZE * sizeof(char));
         // receive access request from client
+        // types of access request: request, terminate
         if (read(named_pipe, buff, SIZE) < 0){
             printf("failed to call read");
             unlink(PIPE);
@@ -55,30 +56,38 @@ int main(void){
 
         sleep(1);
 
-        printf("%s \n", buff);
+        // check if received access request is "request" or not
         if (!(strcmp(buff, "request\n")))
             printf("received request from the client \n");
 
+        // check if received access request is "terminate" or not
         else if (!(strcmp(buff, "terminate\n"))){
             printf("received terminate signal from the client \n");
+
+            // unlink named pipe and then break the loop
             unlink(PIPE);
             printf("----------terminating server----------\n\n");
             break;
         }
 
+        // when neither "request" nor "terminate"
         else{
             printf("wrong request from the client \n");
             continue;
         }
 
-        // receive access mode from client
+        // receiving access mode from client
         if (read(named_pipe, &access_mode, sizeof(char)) < 0) {
+
+            // when it fails
             printf("failed to call read() \n");
+            unlink(PIPE);
             exit(2);
         }
 
         sleep(1);
 
+        // print out the access mode for checking
         printf("received access mode: %c\n", access_mode);
 
         // when the access mode is "r"
@@ -86,71 +95,105 @@ int main(void){
 
             // first of all, receive number of bytes to read
             if (read(named_pipe, &number_of_reading, sizeof(int)) < 0){
+
+                // when it fails, unlink named pipe
                 printf("failed to receive number of bytes to read from client \n");
                 unlink(PIPE);
                 exit(2);
             }
 
+            // print out received number of reading for checking
             printf("received number of bytes to read: %d \n", number_of_reading);
 
+            // receiving file name from the client
             if (read(named_pipe, received_file_name, SIZE) < 0){
+
+                // when it fails
                 printf("failed to receive file name from client \n");
                 continue;
             }
 
             sleep(1);
 
+            // print out received file name for checking
             printf("received file name: %s \n", received_file_name);
 
             // fork child process
             child = fork();
             if (child == 0){
+
+                // file open
                 fd = open(received_file_name, O_RDONLY);
                 if (fd < 0){
+
+                    // when it fails
                     printf("There's no file named %s\n", received_file_name);
 
-                    if (write(named_pipe, "fail\n", 5 * sizeof(char)) < 0)
+                    // send fail signal to the client and then unlink named pipe
+                    if (write(named_pipe, "fail\n", 5 * sizeof(char)) < 0) {
+
+                        // when it fails
+                        unlink(PIPE);
                         exit(3);
+                    }
 
                     sleep(1);
+
+                    unlink(PIPE);
+
                     exit(4);
                 }
 
+                // receiving letters, its length is same with the number of reading, in the file
                 if ((bytes_of_file_read = read(fd, buff, number_of_reading)) < 0){
+
+                    // when it fails, unlink the named pipe
                     printf("failed to read file\n");
                     unlink(PIPE);
                     exit(2);
                 }
 
+                // when the bytes of file read is smaller than number of reading
                 if (bytes_of_file_read < number_of_reading)
                     buff[bytes_of_file_read] = '\0';
 
+                // else case
                 else
                     buff[number_of_reading] = '\0';
 
+                // send letters read from the file to the client
                 if (write(named_pipe, buff, SIZE) < 0){
+
+                    // when it fails, unlink the named pipe
                     printf("failed to send data string\n");
                     unlink(PIPE);
                     exit(3);
                 }
 
                 sleep(1);
+
+                // close file descriptor
                 close(fd);
 
+                // initialization
                 memset(received_file_name, 0, SIZE * sizeof(char));
                 memset(&access_mode, 0, sizeof(char));
                 memset(buff, 0, SIZE * sizeof(char));
                 bytes_of_file_write = 0;
+                bytes_of_file_read = 0;
 
+                // child termination
                 exit(0);
             }
 
+            // when it fails to fork, unlink the named pipe
             else if (child == -1) {
                 printf("failed to fork \n");
                 unlink(PIPE);
                 exit(-1);
             }
 
+            // when it fails to fork, unlink the named pipe
             else {
                 if ((wait(0) == -1)) {
                     printf("failed to fork\n");
@@ -162,40 +205,69 @@ int main(void){
 
         // when the access mode is "w"
         else if (access_mode == 'w'){
+
+            // receiving the file name from the client
             if ((read(named_pipe, received_file_name, SIZE)) < 0){
+
+                // when it fails
                 printf("Failed to receive file name from the client \n");
                 continue;
             }
 
             sleep(2);
 
+            // print out the file name for checking
             printf("received file name from client: %s \n", received_file_name);
 
+
+            // receiving the bytes of file writes from the client
             bytes_of_file_write = read(named_pipe, buff, SIZE);
+
+            // when it fails, unlink named pipe
             if (bytes_of_file_write < 0){
                 printf("Failed to receive data string from the client \n");
                 unlink(PIPE);
                 exit(2);
             }
 
+            // the first status of the bytes of file write is 512
+            // therefore change it to actual bytes of file write
             bytes_of_file_write = strlen(buff);
-            buff[bytes_of_file_write] = '\n';
+            buff[bytes_of_file_write] = '\n'; // change eof to new line
 
+            // fork child process
             child = fork();
             if (child == 0){
+
+                // open file with RW-RW-RW permission (no execution permission)
+                // when it already exists, O_WRONLY
+                // when it doesn't exist, O_CREAT
                 fd = open(received_file_name, O_WRONLY | O_CREAT, 0666);
                 if (fd < 0){
+
+                    // when it fails, send fail signal to client and then unlink the named pipe
                     printf("Failed to write and creat file named %s \n", received_file_name);
-                    if (write(named_pipe, "fail\n", 5 * sizeof(char)) < 0)
+                    if (write(named_pipe, "fail\n", 5 * sizeof(char)) < 0) {
+
+                        // when it fails
+                        unlink(PIPE);
                         exit(3);
+                    }
 
                     sleep(1);
+
+                    unlink(PIPE);
+
                     exit(4);
                 }
 
+                // making file descriptor to point the end of the file
                 lseek(fd, 0, SEEK_END);
 
+                // write on the file
                 if (write(fd, buff, bytes_of_file_write+1) < 0){
+
+                    // when it fails, unlink the named pipe
                     printf("Failed to write data string \n");
                     unlink(PIPE);
                     exit(3);
@@ -203,10 +275,13 @@ int main(void){
 
                 sleep(1);
 
+                // close file descriptor
                 close(fd);
 
-
+                // when it successes, send success signal to the client
                 if (write(named_pipe, "succ\n", 5) < 0) {
+
+                    // when it fails, unlink the named pipe
                     printf("Failed to send number of written bytes \n");
                     unlink(PIPE);
                     exit(3);
@@ -214,9 +289,13 @@ int main(void){
 
                 sleep(1);
 
+                // print out how many bytes are used
                 printf("bytes of file write: %d \n", bytes_of_file_write);
 
+                // send bytes of file write to the client
                 if ((write(named_pipe, &bytes_of_file_write, sizeof(int))) < 0){
+
+                    // when it fails, unlink the named pipe
                     printf("Failed to send number of written bytes \n");
                     unlink(PIPE);
                     exit(3);
@@ -224,6 +303,8 @@ int main(void){
 
                 sleep(1);
 
+
+                // initialization
                 memset(received_file_name, 0, SIZE * sizeof(char));
                 memset(&access_mode, 0, sizeof(char));
                 memset(buff, 0, SIZE * sizeof(char));
@@ -231,15 +312,18 @@ int main(void){
 
                 sleep(1);
 
+                // child termination
                 exit(0);
             }
 
+            // when it fails to fork, unlink the named pipe
             else if (child == -1) {
                 printf("failed to fork \n");
                 unlink(PIPE);
                 exit(-1);
             }
 
+            // when it fails to fork, unlink the named pipe
             else {
                 if ((wait(0) == -1)) {
                     printf("failed to fork\n");
